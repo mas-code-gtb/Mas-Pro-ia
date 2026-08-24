55402a475963e8d6babbd77001b3cab802a7b87e
mas-code-gtb <dioufmassamba440@gmail.com>
2026-08-24 12:12:58 +0200

    Ajouter creation automatique des categories

---
+++ api/management/commands/create_categories.py
@@ -0,0 +1,41 @@
+﻿from django.core.management.base import BaseCommand
+from companies.models import Company
+from products.models import Category
+class Command(BaseCommand):
+    help = 'Cree les categories de produits par defaut si elles n\'existent pas'
+    def handle(self, *args, **options):
+        company = Company.objects.first()
+        if not company:
+            self.stdout.write('Aucune entreprise trouvee. On ignore.')
+            return
+        categories = [
+            "Électronique",
+            "Informatique",
+            "Télécommunications",
+            "Alimentaire",
+            "Boissons",
+            "Vêtements",
+            "Chaussures",
+            "Accessoires",
+            "Mobilier",
+            "Décoration",
+            "Matériaux de construction",
+            "Fournitures de bureau",
+            "Équipement médical",
+            "Pharmacie",
+            "Produits ménagers",
+            "Jardinage",
+            "Auto / Moto",
+            "Services",
+            "Autres",
+        ]
+        created = 0
+        for cat_name in categories:
+            category, created_bool = Category.objects.get_or_create(
+                company=company,
+                name=cat_name,
+                defaults={'description': f'Categorie {cat_name}'}
+            )
+            if created_bool:
+                created += 1
+        self.stdout.write(f'{created} categories creees avec succes.')
